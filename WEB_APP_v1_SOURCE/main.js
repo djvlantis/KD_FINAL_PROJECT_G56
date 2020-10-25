48be9ba7ddc3454b09939b18d88d2fbb93246d24
matercomus <matek.kedzia@gmail.com>
2020-10-25 23:05:46 +0100

    checkpoint

--- WEB_APP_v1_SOURCE/main.js
+++ WEB_APP_v1_SOURCE/main.js
@@ -8,36 +8,37 @@ function mainCtrl($scope, $http) {
 
 
 
-	var country1 = document.getElementById("country_select_1");
-	country1.onchange = function () {
-		console.log(country1.value);
-		$scope.startMyAwesomeApp()
+	// var country1 = document.getElementById("country_select_1");
+	// country1.onchange = function () {
+	// 	console.log(country1.value);
+	// 	$scope.startMyAwesomeApp()
 
 
-	}
+	// }
 
-	var country2 = document.getElementById("country_select_2");
-	country2.onchange = function () {
-		console.log(country2.value);
-		$scope.startMyAwesomeApp()
-	}
+	// var country2 = document.getElementById("country_select_2");
+	// country2.onchange = function () {
+	// 	console.log(country2.value);
+	// 	$scope.startMyAwesomeApp()
+	// }
 
 	$scope.startMyAwesomeApp = function () {
 
 		// graph 1
 		$scope.myDisplayMessage = "";
 		$scope.myDisplayDescription = ""
-		$scope.mySparqlEndpoint = "http://dbpedia.org/sparql";
-		$scope.mySparqlQuery = encodeURI(`PREFIX dbp: <http://dbpedia.org/property/>
-											PREFIX dbo: <http://dbpedia.org/ontology/>
-											PREFIX dbr: <http://dbpedia.org/resource/>
+		$scope.mySparqlEndpoint = "http://192.168.18.4:7200/repositories/repo17";
+		$scope.mySparqlQuery = encodeURI(`PREFIX on:<http://www.example.org/KD/FP/ontology/>
+											PREFIX oon:<http://www.example.org/KD/FP/ontology#>
+											PREFIX owl: <http://www.w3.org/2002/07/owl#>
+											SELECT DISTINCT ?Planet ?Host
+											WHERE 
+											{
+											?Planet a on:Planet;
+													a oon:PotentiallyInhabitablePlanet;
+													on:HasHostName ?Host
 											
-											SELECT DISTINCT ?gdpPerCapitaCountry1 ?gdpPerCapitaCountry2
-												WHERE {
-															dbr:${country1.value} dbp:gdpNominalPerCapita ?gdpPerCapitaCountry1 .
-															dbr:${country2.value} dbp:gdpNominalPerCapita ?gdpPerCapitaCountry2 . 
-													}
-											LIMIT 10`).replace(/#/g, '%23');
+											}LIMIT 10`).replace(/#/g, '%23');
 
 		$http({
 				method: "GET",
@@ -49,16 +50,18 @@ function mainCtrl($scope, $http) {
 			})
 			.success(function (data, status) {
 				$scope.myDynamicLabels = [];
-				$scope.myDynamicgdpPerCapitaCountry1 = [];
-				$scope.myDynamicgdpPerCapitaCountry2 = [];
-
+				$scope.myDynamicPlanet = [];
+				$scope.myDynamicHost = [];
 				// now iterate on the results
 				angular.forEach(data.results.bindings, function (val) {
-					// $scope.myDynamicLabels.push(val.country.value.split('/')[4]);
-					$scope.myDynamicgdpPerCapitaCountry1.push(val.gdpPerCapitaCountry1.value);
-					$scope.myDynamicgdpPerCapitaCountry2.push(val.gdpPerCapitaCountry2.value);
+					// $scope.myDynamicLabels.push(val.country.value.split('/')[3]);
+					$scope.myDynamicPlanet.push(val.Planet.value.split('/')[6]);
+					$scope.myDynamicHost.push(val.Host.value);
 				});
-				$scope.myDynamicData = [$scope.myDynamicgdpPerCapitaCountry1, $scope.myDynamicgdpPerCapitaCountry2]
+				$scope.myDynamicData = [$scope.myDynamicPlanet, $scope.myDynamicHost]
+				console.log($scope.myDynamicPlanet)
+				console.log("myDynamicPlanet")
+
 			})
 			.error(function (error) {
 				console.log('Error running the input query!' + error);
@@ -66,6 +69,6 @@ function mainCtrl($scope, $http) {
 
 
 	};
-
+	$scope.startMyAwesomeApp()
 
 }
